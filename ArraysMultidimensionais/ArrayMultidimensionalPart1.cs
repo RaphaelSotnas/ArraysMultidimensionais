b3d75a10a762a94ffd1f8d501cac5fb397d13c05
Raphael Silva <raphael.silva@targetbank.com.br>
2021-11-18 14:38:43 -0300

    Final

---
+++ ArraysMultidimensionais/ArrayMultidimensionalPart1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace ArraysMultidimensionais
+{
+    class ArrayMultidimensionalPart1
+    {
+        static void Main(string[] args)
+        {
+            AprendendoArraysMultidimencionais();
+            JogoDaVelha();
+        }
+
+        public static void AprendendoArraysMultidimencionais()
+        {
+            //Declaro o tamanho da minha matriz na declaração do meu array(1)
+            string[,] resultados = new string[4, 3];
+            //{
+            //Dessa maneira eu não consigo imprimir no console uma tabela de fato!(0)
+            //Apenas continuo imprimindos times de maneira desordenada.
+            //{ "Alemanha","Espanha","Itália"},
+            //{ "Argentina","Holanda","França"},
+            //{ "Alemanha","Holanda","Brasil" }
+
+
+            //Se faz necessário setar todos os valores do meu array miltidimencional.(3)
+            resultados[0, 0] = "Flamengo";
+            resultados[1, 0] = "Fluminense";
+            resultados[2, 0] = "Vasco";
+            resultados[3, 0] = "Botafogo";
+
+
+
+            resultados[0, 1] = "Atlético";
+            resultados[1, 1] = "Palmeiras";
+            resultados[2, 1] = "São Paulo";
+            resultados[3, 1] = "Vasco";
+
+
+
+            resultados[0, 2] = "Fortaleza";
+            resultados[1, 2] = "Ceará";
+            resultados[2, 2] = "Flamengo";
+            resultados[3, 2] = "Fluminense";
+
+
+            //Mesmo declarando o minha matriz, ainda estou com problemas em "Exibir" a minha tabela.(4)
+
+
+            //}
+
+            //Meu laço foreach não consegue exibir as informações da maneira como eu quero, por isso preciso colocar um laço for.(5)
+            //foreach (var selecao in resultados)
+            //{
+            //    Console.WriteLine(selecao);
+            //}
+
+            for (int jogos = 0; jogos <= resultados.GetUpperBound(1); jogos++)
+            {
+                int ano = 2019 + jogos;
+                //Como o meu resultado é um INT, preciso converter ele para uma string.(6)
+                Console.Write(ano.ToString().PadRight(12));
+            }
+            Console.WriteLine();
+
+            //Para fazer com que a minha tabela seja exibida em forma de tabela(7)
+            //Criei um laço de repetição para exibir meus jogos um abaixo do outro(8)
+            //Coloquei meu 4º colocado na tabela.
+            for (int time = 0; time <= resultados.GetUpperBound(0); time++)
+            {
+                for (int ano = 0; ano <= resultados.GetUpperBound(1); ano++)
+                {
+                    Console.Write(resultados[time, ano].PadRight(12));
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public static void JogoDaVelha()
+        {
+            string[,] tabuleiro = new string[3, 3];
+
+            tabuleiro[0, 0] = "x";
+            tabuleiro[1, 0] = " ";
+            tabuleiro[2, 0] = "O";
+
+            tabuleiro[0, 1] = "X";
+            tabuleiro[1, 1] = "X";
+            tabuleiro[2, 1] = "O";
+
+            tabuleiro[0, 2] = " ";
+            tabuleiro[1, 2] = "O";
+            tabuleiro[2, 2] = "X";
+
+            for (var coluna = 0; coluna <= tabuleiro.GetUpperBound(0); coluna++)
+            {
+                for (var linha = 0; linha <= tabuleiro.GetUpperBound(1); linha++)
+                {
+                    Console.Write(tabuleiro[linha, coluna].PadRight(2));
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+}
---
+++ JogoDaVelha/JogoDaVelha.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    class JogoDaVelha
+    {
+        //Preciso criar as etapas de um jogo e pensar na lógica para que ele aconteça
+
+        private bool fimDeJogo;
+        private string[] posicoes;
+        private string vez;
+        private int quantidadeDeJogadas = 0;
+
+        public JogoDaVelha()
+        {
+            fimDeJogo = false;
+            posicoes = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            vez = "X";
+        }
+
+        public void iniciarJogo()
+        {
+            //Enquanto o fim de jogo não chega, o que deve ser feito?
+            while (!fimDeJogo)
+            {
+                //Preciso atualizar a tabela para que o meu jogador escolha um número para começar o jogo.
+                AtualizarTabela();
+
+                //Após isso, preciso ler a escolha que ele fez 
+                LerEscolhaDoJogador();
+
+                //Depois disso eu mostro a tabela com o valor escolhido.
+                AtualizarTabela();
+
+                //Verificar Se alguém ganhou o game.
+                VerificarVencedor();
+
+                //Dar a vez ao outro jogador
+                MudarVez();
+
+            }
+
+            Console.WriteLine("Temos um vencedor! Parabéns!");
+        }
+
+        private void MudarVez()
+        {
+            if(vez == "X")
+            {
+                vez = "O";
+            }
+            else
+            {
+                vez = "X";
+            }
+        }
+
+        private void VerificarVencedor()
+        {
+            VencedorLinha();
+            VencedorColuna();
+            VencedorInclinado();
+            if (VencedorLinha()== true || VencedorColuna() || VencedorInclinado() == true)
+            {
+                fimDeJogo = true;
+                Console.WriteLine($"Fim de jogo! Vitória do {vez}");
+            }
+
+            if(quantidadeDeJogadas == 9)
+            {
+                fimDeJogo = true;
+                Console.WriteLine("EMPATE! Tente novamente");
+            }
+
+        }
+
+        private bool VencedorLinha()
+        {
+            bool vitoriaLinha1 = posicoes[0] == posicoes[1] && posicoes[1] == posicoes[2];
+            bool vitoriaLinha2 = posicoes[3] == posicoes[4] && posicoes[4] == posicoes[5];
+            bool vitoriaLinha3 = posicoes[6] == posicoes[7] && posicoes[7] == posicoes[8];
+
+            return vitoriaLinha1 || vitoriaLinha2 || vitoriaLinha3;
+        }
+
+        private bool VencedorColuna()
+        {
+            bool vitoriaColuna1 = posicoes[0] == posicoes[3] && posicoes[3] == posicoes[6];
+            bool vitoriaColuna2 = posicoes[1] == posicoes[4] && posicoes[4] == posicoes[7];
+            bool vitoriaColuna3 = posicoes[2] == posicoes[5] && posicoes[5] == posicoes[8];
+            return vitoriaColuna1 || vitoriaColuna2 || vitoriaColuna3;
+
+        }
+
+        private bool VencedorInclinado()
+        {
+            bool vitoriaInclinado1 = posicoes[0] == posicoes[4] && posicoes[4] == posicoes[8];
+            bool vitoriaInclinado2 = posicoes[2] == posicoes[4] && posicoes[4] == posicoes[6];
+            return vitoriaInclinado1 || vitoriaInclinado2;
+
+        }
+
+        private void LerEscolhaDoJogador()
+        {
+            Console.WriteLine($"Agora é a vez de {vez} jogar, escolha um número disponível na tabela.");
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            int numeroNoArray = escolha - 1;
+            posicoes[numeroNoArray] = vez;
+            quantidadeDeJogadas++;
+;       }
+
+        private void AtualizarTabela()
+        {
+            Console.Clear();
+            Console.WriteLine(ObterTabela());
+
+        }
+        private string ObterTabela()
+        {
+            return $"___{posicoes[0]}__|___{posicoes[1]}__|___{posicoes[2]}__\n" +
+                   $"___{posicoes[3]}__|___{posicoes[4]}__|___{posicoes[5]}__\n" +
+                   $"___{posicoes[6]}__|___{posicoes[7]}__|___{posicoes[8]}__\n";
+        }
+    }
+}
